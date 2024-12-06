f6f496601011e7059137a3be35c164ffed564747
Damian Koziołek <damikozi223@gmail.com>
2024-12-06 23:21:04 +0100

    feat(stub): change and set hasConsoleCommands

--- src/StuberatorServiceProvider.php
+++ src/StuberatorServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 namespace Record\Stuberator;
 
+use Record\Stuberator\Console\Commands\MakeAction;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Spatie\LaravelPackageTools\Package;
@@ -11,16 +12,6 @@ class StuberatorServiceProvider extends PackageServiceProvider
     {
         $package->name('stuberator')
             ->publishesServiceProvider('StuberatorServiceProvider')
-            ->hasConsoleCommands([
-                \Record\Stuberator\Console\Commands\MakeAction::class,
-                \Record\Stuberator\Console\Commands\MakeService::class,
-                \Record\Stuberator\Console\Commands\MakeEnum::class
-            ])
-            ->hasInstallCommand(function(InstallCommand $command) {
-                $command
-                    ->copyAndRegisterServiceProviderInApp();
-            });;
-
-
+            ->hasCommand(MakeAction::class);
     }
 }
